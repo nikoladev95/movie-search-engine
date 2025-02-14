ba01558eced554a3c4809a2daf2e3ced386e7398
Nikola Milanovic <nikola.milanovic.dev@gmail.com>
2025-02-14 18:26:52 +0100

    Header with search complete.

--- movie-search-engine/src/App.jsx
+++ movie-search-engine/src/App.jsx
@@ -1,11 +1,21 @@
+import Search from "./components/Search"
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   )
 }
 
